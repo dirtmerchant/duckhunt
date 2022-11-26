531241ba91f4756b4ba574e436a2d8cef3ad3f8a
Dirtmerchant <bert@bertbullough.com>
2022-11-26 11:15:23 -0700

    adds draw gun function

--- venv/main.py
+++ venv/main.py
@@ -11,11 +11,43 @@ screen = pygame.display.set_mode([WIDTH, HEIGHT])
 bgs = []
 banners = []
 guns = []
-level = 3
+level = 1
 for i in range(1, 4):
-    bgs.append(pygame.image.load(f"assets/bgs/{1}.png"))
+    bgs.append(pygame.image.load(f"assets/bgs/{i}.png"))
     banners.append(pygame.image.load(f"assets/banners/{i}.png"))
     guns.append(pygame.image.load(f"assets/guns/{i}.png"))
+
+
+def draw_gun():
+    mouse_pos = pygame.mouse.get_pos()
+    gun_point = (WIDTH / 2, HEIGHT - 200)
+    lasers = ["red", "purple", "green"]
+    clicks = pygame.mouse.get_pressed()
+    if mouse_pos[0] != gun_point[0]:
+        slope = mouse_pos[1] - gun_point[1] / (mouse_pos[0] - gun_point[0])
+    else:
+        slope = -100000
+    angle = math.atan(slope)
+    rotation = math.degrees(angle)
+    if mouse_pos[0] < WIDTH / 2:
+        gun = pygame.transform.flip(guns[level - 1], True, False)
+        if mouse_pos[1] < 600:
+            screen.blit(
+                pygame.transform.rotate(gun, 90 - rotation),
+                (WIDTH / 2 - 90, HEIGHT - 250),
+            )
+            if clicks[0]:
+                pygame.draw.circle(screen, lasers[level - 1], mouse_pos, 5)
+    else:
+        gun = guns[level - 1]
+        if mouse_pos[1] < 600:
+            screen.blit(
+                pygame.transform.rotate(gun, 270 - rotation),
+                (WIDTH / 2 - 30, HEIGHT - 250),
+            )
+            if clicks[0]:
+                pygame.draw.circle(screen, lasers[level - 1], mouse_pos, 5)
+
 
 run = True
 while run:
@@ -24,6 +56,9 @@ while run:
     screen.fill("black")
     screen.blit(bgs[level - 1], (0, 0))
     screen.blit(banners[level - 1], (0, HEIGHT - 200))
+
+    if level > 0:
+        draw_gun()
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
