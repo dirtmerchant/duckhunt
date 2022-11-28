033c7184e792b4e475c7962fd8d204730b68c0f6
Dirtmerchant <bert@bertbullough.com>
2022-11-28 09:17:54 -0700

    adds target removal

--- venv/main.py
+++ venv/main.py
@@ -11,9 +11,16 @@ screen = pygame.display.set_mode([WIDTH, HEIGHT])
 bgs = []
 banners = []
 guns = []
+points = 0
 target_images = [[], [], []]
 targets = {1: [10, 5, 3], 2: [12, 8, 5], 3: [15, 12, 8, 3]}
-level = 2
+level = 3
+
+shot = False
+total_shots= 0
+mode = 0
+ammo = 0
+
 one_coords = [[], [], []]
 two_coords = [[], [], []]
 three_coords = [[], [], [], []]
@@ -104,6 +111,16 @@ def draw_level(coords):
             screen.blit(target_images[level - 1][i], coords[i][j])
     return target_rects
 
+def check_shot(targets, coords):
+    global points
+    mouse_pos = pygame.mouse.get_pos()
+    for i in range(len(targets)):
+        for j in range(len(targets[i])):
+            if targets[i][j].collidepoint(mouse_pos):
+                coords[i].pop(j)
+                points += 10 + 10 * (i**2)
+                # add sounds for enemy hit
+    return coords
 
 # initialize enemy coordinates
 
@@ -137,10 +154,19 @@ while run:
     if level == 1:
         target_boxes = draw_level(one_coords)
         one_coords = move_level(one_coords)
+        if shot:
+            one_coords = check_shot(target_boxes, one_coords)
+            shot = False
     elif level == 2:
         target_boxes = draw_level(two_coords)
         two_coords = move_level(two_coords)
+        if shot:
+            two_coords = check_shot(target_boxes, two_coords)
+            shot = False
     elif level == 3:
+        if shot:
+            three_coords = check_shot(target_boxes, three_coords)
+            shot = False
         target_boxes = draw_level(three_coords)
         three_coords = move_level(three_coords)
 
@@ -150,6 +176,13 @@ while run:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
+        if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+            mouse_position = pygame.mouse.get_pos()
+            if (0 < mouse_position[0] < WIDTH) and (0 < mouse_position[1] < HEIGHT - 200):
+                shot = True
+                total_shots += 1
+                if mode == 1:
+                    ammo -= 1
 
     pygame.display.flip()
 pygame.quit()
